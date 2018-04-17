1bca5f1fefc9702af84aa8d78b8219595a1f8ac4
ShanePlater <shaneplater92@gmail.com>
2018-04-17 22:36:12 +1000

    adding sql service
    adding in server connection initialiser

--- ASPNet.Core/Models/DataService.cs
+++ ASPNet.Core/Models/DataService.cs
@@ -2,15 +2,78 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNet;
+using MySql.Data.MySqlClient;
 
 namespace ASPNet.Core.Models
 {
     
     public class DataService
     {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
         public DataService()
         {
             // Code to initialise mysql connection
+        }
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "mikamusicstudio";
+            uid = "root";
+            password = "password";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        //MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                       // MessageBox.Show("Invalid username/password, please try again");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+               // MessageBox.Show(ex.Message);
+                return false;
+            }
         }
 
         public List<Student> getStudents()
