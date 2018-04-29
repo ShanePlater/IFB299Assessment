5fbaae2595cc047919e0198bebed06f0550767a8
Eric T <ericson.t@hotmail.com>
2018-04-29 18:50:20 +1000

    Register Front-end complete
    Frontend dev for register page.
    Note: Not designed suitable for smaller resolutions.

--- GMS.ASPNet.Core/Models/SessionViewModels/RegisterViewModel.cs
+++ GMS.ASPNet.Core/Models/SessionViewModels/RegisterViewModel.cs
@@ -24,6 +24,11 @@ namespace GMS.ASPNet.Core.Models.SessionViewModels
         public string Email { get; set; }
 
         [Required]
+        [EmailAddress]
+        [Display(Name = "Contact Number")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
