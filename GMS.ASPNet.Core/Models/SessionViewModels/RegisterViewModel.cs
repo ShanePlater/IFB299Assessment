448df596273585619a3e7d661a6f7cab00d87020
Eric T <ericson.t@hotmail.com>
2018-04-29 15:05:34 +1000

    register front-end shit
    switching to laptop

--- GMS.ASPNet.Core/Models/SessionViewModels/RegisterViewModel.cs
+++ GMS.ASPNet.Core/Models/SessionViewModels/RegisterViewModel.cs
@@ -15,6 +15,7 @@ namespace GMS.ASPNet.Core.Models.SessionViewModels
 
         [Required]
         [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
         public DateTime DOB { get; set; }
 
         [Required]
@@ -23,14 +24,14 @@ namespace GMS.ASPNet.Core.Models.SessionViewModels
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
-        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        [Compare("Password", ErrorMessage = "The passwords do not match.")]
         public string ConfirmPassword { get; set; }
 
 
